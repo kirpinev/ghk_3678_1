cc80aaf563a9c7c70c69747642e29306abd328e1
Igor Kirpinev <kirpinevigor@yandex.ru>
2025-04-28 13:22:19 +0300

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import "swiper/css";
 import { App } from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
